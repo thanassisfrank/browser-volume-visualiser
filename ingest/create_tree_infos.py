8781bacfa8907b3b346610d7bad584619311035a
Thanassis Frank <thanassisfrank@gmail.com>
2025-03-07 13:30:53 +0000

    Modify batch system to take json job descriptions

--- ingest/create_tree_infos.py
+++ ingest/create_tree_infos.py
@@ -1,31 +1,80 @@
 import subprocess
 import os
 import argparse
+import json
 
-cell_counts = [64, 128, 256, 512, 1024, 2048]
 
-def main():
-    parser = argparse.ArgumentParser(prog="generate_block_mesh")
-    parser.add_argument("file-path", help="path to the cgns file to process")
-    parser.add_argument("out", help="path to the cgns file to process")
+def run_job(job, force=False, verbose=False):
 
-    args = vars(parser.parse_args())
+    job_cmd_parts = ["python3", "./generate_block_mesh.py", job["file"], "-e"] 
 
-    for i, cells in enumerate(cell_counts):
-        out_dir = args["out"] + "_" + str(cells)
-        print("running %i/%i" % (i + 1, len(cell_counts)))
+    if job.get("decimate"): 
+        job_cmd_parts.extend(["--decimate", str(job["decimate"])])
+    if job.get("size"): 
+        job_cmd_parts.extend(["--size-x", str(job["size"][0])])
+        job_cmd_parts.extend(["--size-y", str(job["size"][1])])
+        job_cmd_parts.extend(["--size-z", str(job["size"][2])])
+    if job.get("type"):
+        job_cmd_parts.extend(["--data-type", job["type"]])
+    if job.get("noFiles"):
+        job_cmd_parts.extend(["-n"])
+    if job.get("verbose"):
+        job_cmd_parts.extend(["-v"])
+    if job.get("scalars"):
+        job_cmd_parts.extend(["-s", *job["scalars"]])
+        
+    
+    for i, cells in enumerate(job["cells"]):
+        out_dir = job["out"] + "_" + str(cells)
+        print("running task %i/%i" % (i + 1, len(job["cells"])))
         try:
             os.mkdir(out_dir)
         except FileExistsError:
-            print("%s exists, skipping..." % out_dir)
-            continue
+            if not force:
+                print("%s exists, skipping..." % out_dir)
+                continue
         
         print("processing %s..." % out_dir)
         out_path = out_dir + "/"
-        subprocess.run(
-            "python generate_block_mesh.py %s -e -n -s none -o %s -c %s" % 
-            (args["file-path"], out_path, cells)
-        )
+
+        this_job_cmd_parts = job_cmd_parts[:]
+        this_job_cmd_parts.extend(["-o", out_path])
+        this_job_cmd_parts.extend(["-c", str(cells)])
+
+        cmd =  " ".join(this_job_cmd_parts)
+
+        if verbose: print("cmd: " + cmd)
+
+        subprocess.run(cmd, shell=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="generate_block_mesh")
+    parser.add_argument("--file", help="path to the cgns file to process")
+    parser.add_argument("--out", help="path to the cgns file to process")
+    parser.add_argument("--json", default=None, help="path to json job file")
+    parser.add_argument("-v", action="store_true", help="verbose switch")
+    parser.add_argument("-f", action="store_true", help="run task even if output folder exists")
+
+    args = vars(parser.parse_args())
+
+    # create the jobs
+    if args["json"] is not None:
+        # get jobs from json
+        with open(args["json"]) as file:
+            jobs = json.loads(file.read())
+    else:
+        # construct a default job from the input
+        jobs = [{
+            file: args["file"],
+            out: args["out"],
+            cells: [2048, 1024, 512, 256, 128]
+        }]
+
+    for i, job in enumerate(jobs):
+        print("running job %i/%i" % (i + 1, len(jobs)))
+        run_job(job, args["f"], args["v"])
+
 
 if __name__ == "__main__":
     main()
