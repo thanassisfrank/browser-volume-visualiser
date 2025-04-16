e437f558aeae162a2cb629086654e02c72a1a26f
Thanassis Frank <thanassisfrank@gmail.com>
2025-04-16 17:21:00 +0100

    Fix frame time graph not showing

--- static/index.js
+++ static/index.js
@@ -107,7 +107,7 @@ const renderModeFromOpts = (opts) => {
 
 // define the main function
 async function main() {
-    const app = new App(get("c"), 1, get("frame-time-graph"));
+    const app = new App(get("c"), {frametimeCanvas: get("frame-time-graph")});
     await app.init();
 
     // setup the options elements
